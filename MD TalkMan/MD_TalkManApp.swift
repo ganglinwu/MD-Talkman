8fec98e390563287b5f0ae31f0198887e55dd0f1
ganglinwu <ganglin.wu@gmail.com>
2025-08-06 10:36:05 +0800

    Initial Commit

---
+++ MD TalkMan/MD_TalkManApp.swift
@@ -0,0 +1,17 @@
+//
+//  MD_TalkManApp.swift
+//  MD TalkMan
+//
+//  Created by Ganglin Wu on 6/8/25.
+//
+
+import SwiftUI
+
+@main
+struct MD_TalkManApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ MD TalkManTests/MD_TalkManTests.swift
@@ -0,0 +1,17 @@
+//
+//  MD_TalkManTests.swift
+//  MD TalkManTests
+//
+//  Created by Ganglin Wu on 6/8/25.
+//
+
+import Testing
+@testable import MD_TalkMan
+
+struct MD_TalkManTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
